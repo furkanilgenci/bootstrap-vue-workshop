cd7821ca912036e107ab3db0698cdad0d3c1df66
furkan <furkanilgenci@gmail.com>
2022-05-19 16:10:28 +0300

    Implement responsive cards

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CarCard from './components/CarCard.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      cars: [
+        { name: 'audi a3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+        { name: 'BMW i8', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+        { name: 'Ferrari', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+        { name: 'Mercedes E220', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+        { name: 'Masserati', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+        { name: 'Jaguar', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+        { name: 'Toyota', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+        { name: 'Honda', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+      ]
+    }
+  },
+  async created() {
+    // const res = await ....
+    // this.cars = res.data;
+  },
   components: {
-    HelloWorld
+    CarCard
   }
 }
 </script>
 
+<template lang="pug">
+  #app
+    h2.available-cars-title Available cars
+    .container
+      .row
+        .col-12.col-lg-6.col-xl-4(v-for="car in cars")
+          CarCard(:name="car.name" :description="car.description")
+  </div>
+</template>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 24px;
+}
+
+.available-cars-title {
+  color: green;
+  margin-bottom: 24px;
 }
 </style>
---
+++ src/components/CarCard.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.card-container
+  h3 {{ name }}
+  p {{ description }}
+  button.btn.btn-primary Rent
+
+</template>
+
+<script>
+export default {
+  name: 'CarCard',
+  props: {
+    name: String,
+    description: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card-container {
+  border: 1px solid #d0d0d0;
+  padding: 12px;
+  margin-bottom: 24px;
+  border-radius: 8px;
+}
+</style>
